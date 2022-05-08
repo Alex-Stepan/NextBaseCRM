61280990e8172e6c1cbb654c145bdade74862fdd
BLSoftUser <bluelaser.soft@gmail.com>
2022-05-07 22:14:00 -0400

    Framework is created

---
+++ src/test/java/com/nextBaseCrm/utilities/ConfigReader.java
@@ -0,0 +1,29 @@
+package com.nextBaseCrm.utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+    /** 1. Create obj for the Static Block */
+    private static Properties properties = new Properties();
+
+
+    /** 2. Static Block, for catching exceptions */
+    static {
+        try {
+            FileInputStream fileIn = new FileInputStream("config.properties");
+            properties.load(fileIn);
+            fileIn.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    /** 3. to get the value from properties */
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+
+
+}
